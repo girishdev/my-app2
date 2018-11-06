1a5b1deffb45378f90d50eeaca601a7c12564c22
girishdev <girishkumar.aorg@gmail.com>
2018-11-06 15:08:59 +0530

    first commit

---
+++ Notes.php
@@ -0,0 +1,9 @@
+Sibling elements should not be there
+App.vue must be leaner
+
+Components UseFull Links:-
+    https://vuejs.org/v2/guide/components.html
+    https://vuejs.org/v2/guide/components-registration.html
+
+Parent to Child passding data:
+    "Props" meaning propertys
